13db17a4698047f378f09d1f378bd559743d1535
victor <info@victorhug.de>
2023-12-05 19:41:50 +0100

    A new beginning, and the continuation of the deployment development

---
+++ app/lib/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SecretsTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      primarySwatch: Colors.red,
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      primarySwatch: Colors.red,
+    );
+  }
+}
