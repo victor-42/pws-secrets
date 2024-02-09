e0c4109db604f9a5cda4c1e7a4b7fc5994f75b5f
Victor Hug <info@victorhug.de>
2024-02-09 02:36:37 +0100

    Key rotation text & button

--- app/lib/screens/home.dart
+++ app/lib/screens/home.dart
@@ -3,12 +3,10 @@ import 'dart:html';
 
 import 'package:flutter/material.dart';
 
-const rotationText = 'The key-rotation allows you to rotate the key used by the server for encrypting your messages. \n'
+const rotationText =
+    'The key-rotation allows you to rotate the key used by the server for encrypting your messages. \n'
     'It is only possible once every 72 hours, since unopened secrets still need to be encryptable. \n'
     'For obvious reasons, you cannot set a specific encryption key, but a seed function will come in the future.';
-
-
-
 
 class LastSecretsScreen extends StatefulWidget {
   const LastSecretsScreen({Key? key}) : super(key: key);
@@ -20,6 +18,7 @@ class LastSecretsScreen extends StatefulWidget {
 class LastSecretsScreenState extends State<LastSecretsScreen> {
   int _currentTimerTime = -10;
   Timer? _timer;
+  bool _isRotating = false;
 
   @override
   void dispose() {
@@ -81,7 +80,8 @@ class LastSecretsScreenState extends State<LastSecretsScreen> {
             const Divider(),
             const SizedBox(height: 20),
             // Key Rotation
-            Text('Key Rotation', style: Theme.of(context).textTheme.headlineSmall),
+            Text('Key Rotation',
+                style: Theme.of(context).textTheme.headlineSmall),
             const SizedBox(height: 10),
             Text(rotationText, style: Theme.of(context).textTheme.bodyMedium),
             const SizedBox(height: 20),
@@ -102,21 +102,32 @@ class LastSecretsScreenState extends State<LastSecretsScreen> {
                       Icon(Icons.timer),
                       SizedBox(width: 10),
                       Text('${_currentTimerTime <= 0 ? '-' : '+'} '
-                          '${_currentTimerTime.abs()~/86400}:'
-                          '${_currentTimerTime.abs()~/3600%24}:'
-                          '${_currentTimerTime.abs()~/60%60}:'
-                          '${_currentTimerTime.abs()%60}'),
+                          '${(_currentTimerTime.abs() ~/ 86400).toString().padLeft(2, '0')}:'
+                          '${(_currentTimerTime.abs() ~/ 3600 % 24).toString().padLeft(2, '0')}:'
+                          '${(_currentTimerTime.abs() ~/ 60 % 60).toString().padLeft(2, '0')}:'
+                          '${(_currentTimerTime.abs() % 60).toString().padLeft(2, '0')}'),
                     ],
                   ),
-                  ElevatedButton(
-                    onPressed: _currentTimerTime > 0 ? () {} : null,
-                    child: const Text('Request Rotation'),
-
+                  SizedBox(
+                    width: 150,
+                    height: 30,
+                    child: ElevatedButton(
+                      onPressed: _currentTimerTime > 0 && !_isRotating
+                          ? () {
+                              _isRotating = true;
+                            }
+                          : null,
+                      child: _isRotating
+                          ? SizedBox(
+                              height: 15,
+                              width: 15,
+                              child: const CircularProgressIndicator())
+                          : const Text('Request Rotation'),
+                    ),
                   )
                 ],
               ),
-              ),
-
+            ),
           ],
         ),
       ),
